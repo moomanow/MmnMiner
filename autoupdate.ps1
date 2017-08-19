67740768fdc4549ab07067cc47e28978d5598fe5
viatoro <jarupong.wiboonchart@gmail.com>
2017-08-19 13:04:30 +0700

    add check currentVersion

--- autoupdate.ps1
+++ autoupdate.ps1
@@ -4,10 +4,13 @@ function autoupdate {
         [Switch]$autoupdate = $false
     )
     if($autoupdate){
+        $currentVersion = git rev-parse HEAD
         $lastVersion = git rev-list --tags --max-count=1
-        $latestTag =git describe --tags $lastVersion
-        git checkout $latestTag
-        start run.bat
-        exit 0
+        if($currentVersion -ge $lastVersion){
+            $latestTag =git describe --tags $lastVersion
+            git checkout $latestTag
+            start run.bat
+            exit 0
+        }
     }
 }
